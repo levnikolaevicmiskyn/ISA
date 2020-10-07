54bb711c4c355a65217fbed5d987884d04bf236d
Alessandro Scisca <s276032@studenti.polito.it>
2020-10-07 18:55:43 +0000

    Multiplier entity

--- lab1/project/src/multiplier.vhd
+++ lab1/project/src/multiplier.vhd
@@ -1,5 +1,11 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
 
-
-
-
- 
+entity multiplier is
+    generic (N_BIT: natural);
+    port (
+        a, b:   in  signed(N_BIT-1 downto 0);
+        y:      out signed(N_BIT-1 downto 0)
+    );
+end entity multiplier;
