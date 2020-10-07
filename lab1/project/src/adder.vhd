e7741ad79204cf10d6b7ad94904ee35459b4f093
Gabriele Perrone <lele.gabri1997@gmail.com>
2020-10-08 01:27:36 +0200

    finished multiplier and adder

--- lab1/project/src/adder.vhd
+++ lab1/project/src/adder.vhd
@@ -1,26 +1,30 @@
 library ieee; 
 use ieee.std_logic_1164.all;
+use IEEE.numeric_std.all;
 
 entity adder is
 	generic	(
-				N_BIT 	: positive
+				N_BIT 	: natural							--adder parallelism
 			);
 	port(
-			a			: in signed(N_BIT-1 downto 0);
-			b			: in signed(N_BIT-1 downto 0);
-			carry_in	: in std_logic;
-			sum			: out signed(N_BIT-1 downto 0);
-			carry_out	: out std_logic;
+			a			: in signed(N_BIT-1 downto 0);		--first operand
+			b			: in signed(N_BIT-1 downto 0);		--second operand
+			carry_in	: in std_logic;						--input carry
+			sum			: out signed(N_BIT-1 downto 0);		--output sum
+			carry_out	: out std_logic						--output carry
 		);
 end entity adder;
 		
 architecture behavioral of adder is
 
 	signal temp_sum: signed(N_BIT downto 0);
+	signal temp_carry_in: std_logic_vector(1 downto 0);
 
 begin
-
-	temp_sum <= a + b + signed(carry_in);
+	
+	temp_carry_in <= '0' & carry_in;
+	
+	temp_sum <= (a(N_BIT-1) & a) + (b(N_BIT-1) & b) + signed(temp_carry_in);
 	
 	sum <= temp_sum(N_BIT-1 downto 0);
 	
--- lab1/project/src/multiplier.vhd
+++ lab1/project/src/multiplier.vhd
@@ -6,6 +6,17 @@ entity multiplier is
     generic (N_BIT: natural);
     port (
         a, b:   in  signed(N_BIT-1 downto 0);
-        y:      out signed(N_BIT-1 downto 0)
+        y:      out signed(N_BIT-1 downto 0)  	
     );
 end entity multiplier;
+
+architecture behavioral of multiplier is
+
+	signal temp_y : signed(2*N_BIT-1 downto 0);	
+												
+begin
+
+	temp_y <= a*b;
+	y <= temp_y(2*N_BIT-1 downto N_BIT);
+
+end architecture behavioral;
