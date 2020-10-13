6b151cd5b61a9832f96a4ec4d1e2d0cda8d5c695
levnikolaevicmiskyn <37081598+levnikolaevicmiskyn@users.noreply.github.com>
2020-10-13 09:51:22 +0200

    dataGen corretto

--- lab1/project/tb/dataGen.vhd
+++ lab1/project/tb/dataGen.vhd
@@ -1,6 +1,9 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_textio.all;
+use ieee.numeric_std.all;
+--use ieee.std_logic_arith.all;
+
 
 library std;
 use std.textio.all;
@@ -8,32 +11,30 @@ use std.textio.all;
 entity dataGen is
 	generic(NB : INTEGER := 8);
 	port(clk, rst_n : in std_logic;
-		data_out : in signed(NB-1 downto 0);
+		data_out : out signed(NB-1 downto 0);
 		end_sim, vout: out std_logic
 	);
 end dataGen;
 
 architecture behavior of dataGen is
-begin
-
-constant tco : time := 1 ns -- Clock to output delay
+constant tco : time := 1 ns; -- Clock to output delay
 signal end_sim_s : std_logic;
-signal end_sim_v : std_logic_vector(9 downto 0);
-
+signal end_sim_v : std_logic_vector(10 downto 0);
+begin
 fetch_proc: process (clk, rst_n)
 	file samplefile : text open READ_MODE is "samples.txt";
 	variable linein : line;
 	variable x : integer;
 begin
 	if rst_n = '0' then
-		dout <= (others => '0') after tco;
+		data_out <= (others => '0') after tco;
 		vout <= '0' after tco;
 		end_sim_s <= '0' after tco;
 	elsif rising_edge(clk) then
 		if not endfile(samplefile) then
 			readline(samplefile, linein); -- Read a new line from the input file
 			read(linein, x); -- Interpret the text line as an integer and store it in xn
-			data_out <= std_logic_vector(to_unsigned(x, x'LENGTH)) after tco;
+			data_out <= to_signed(x, data_out'LENGTH) after tco;
 			vout <= '1' after tco;
 		else
 			vout <= '0' after tco;
