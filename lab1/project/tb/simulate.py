fb3809cfdb48ae13ae9f63512599a37b0912177c
Alessandro Scisca <s276032@studenti.polito.it>
2020-10-20 09:09:09 +0000

    Fix simulation length

--- lab1/project/tb/simulate.py
+++ lab1/project/tb/simulate.py
@@ -25,8 +25,6 @@ def generate_inputs(time, scale):
     Return:
         numpy.array: simulation samples
     """
-    #TODO fix input generation
-    return np.array([64]*len(time))
     t1 = time[-1]
     f0 = 10
     f1 = 5000
@@ -53,15 +51,14 @@ def run_bash(command, **kwargs):
     return status
 
 
-def run_simulation(time, simulation_script):
-    duration = int(time[-1] * 1e9)
+def run_simulation(duration, simulation_script):
     duration = f"{duration} ns"
     # Delete work folder
     delete_folder = "rm -rf ./work/"
     run_bash(delete_folder)
     # Run simulation
     simulation_command = f"source /software/scripts/init_msim6.2g && " \
-                         f"source ./simulate.do \"{duration}\" |& sed 's/^/    /'"
+                         f"source {simulation_script} \"{duration}\" |& sed 's/^/    /'"
     status = run_bash(simulation_command)
     return status
 
@@ -81,30 +78,30 @@ def run_reference(executable, samples, results):
 
 
 def compare_results(file_c, file_vhdl, threshold):
-	"""Compare simulation results.
-	Args:
-		file_c (file): reference results
-		file_vhdl (file): simulation results
-		threshold (int): minimum error to consider a failure
-	Return:
-		int: exit code
-	"""
-	# Check file lengths
-	lenc = sum(1 for line in file_c)
-	lenv = sum(1 for line in file_vhdl)
-	if lenc != lenv:
-		print(f"  Files differ in length: C = {lenc}; VHDL = {lenv}")
-		return 2
-	# Compare numerical results
-	for line, (resc, resv) in enumerate(zip(file_c, file_vhdl)):
-		resc = int(resc)
-		resv = int(resv)
-		if abs(resc - resv) > threshold:
-			print(f"  Threshold crossed on line {line}: C = {resc}; VHDL = {resv}")
-			return 3
-	return 0
-	
-	
+    """Compare simulation results.
+    Args:
+        file_c (file): reference results
+        file_vhdl (file): simulation results
+        threshold (int): minimum error to consider a failure
+    Return:
+        int: exit code
+    """
+    # Check file lengths
+    lenc = sum(1 for line in file_c)
+    lenv = sum(1 for line in file_vhdl)
+    if lenc != lenv:
+        print(f"  Files differ in length: C = {lenc}; VHDL = {lenv}")
+        return 2
+    # Compare numerical results
+    for line, (resc, resv) in enumerate(zip(file_c, file_vhdl)):
+        resc = int(resc)
+        resv = int(resv)
+        if abs(resc - resv) > threshold:
+            print(f"  Threshold crossed on line {line}: C = {resc}; VHDL = {resv}")
+            return 3
+    return 0
+
+
 def color(string, code):
     """Surround a string with ANSI escape sequences to color outputs.
     Args:
@@ -123,7 +120,8 @@ def status_update(status, error=''):
         error (str): generic error message
     Return:
         function: wrapped function
-	"""
+    """
+
     def decorate(func):
         def decorated_func(*args, **kwargs):
             print(color(status, 'OKBLUE'))
@@ -133,13 +131,22 @@ def status_update(status, error=''):
                 exit(code)
             else:
                 print(color("  Done!", 'OKGREEN'))
+
         return decorated_func
+
     return decorate
 
 
 def main():
     """Main function"""
     time = None
+    input_duration = 1e-3
+    input_delta = 1 / 10e3
+    input_nsamples = input_duration / input_delta
+
+    simulation_delta = 1e-9
+    simulation_duration = (30 + input_nsamples) * simulation_delta
+
     # Clean simulation related files
     @status_update("Cleaning files...")
     def _clean_files():
@@ -150,17 +157,19 @@ def main():
         except OSError:
             pass
         return 0
+
     _clean_files()
-    
+
     # Generate time ans input samples
     @status_update("Generating inputs...")
     def _generate_inputs():
         nonlocal time
-        time = np.arange(0.0, 501e-9, 1e-9)
+        time = np.crange(0.0, input_duration, input_delta)
         inputs = generate_inputs(time, 128)
         # Store samples in a file
         np.savetxt('samples.txt', inputs, fmt='%d')
         return 0
+
     _generate_inputs()
 
     # Launch reference filter
@@ -168,14 +177,16 @@ def main():
     def _reference():
         status = run_reference('./cfilter.out', 'samples.txt', 'results-C.txt')
         return status.returncode
+
     _reference()
 
     # Launch VHDL simulation
     @status_update("Simulating VHDL...", "Simulation unsuccesful")
     def _simulate():
         nonlocal time
-        status = run_simulation(time, 'simulate.do')
+        status = run_simulation(simulation_duration, './simulate.do')
         return status.returncode
+
     _simulate()
 
     # Check simulation results
@@ -183,6 +194,7 @@ def main():
     def _check_results():
         with open("results-C.txt", 'r') as file_c, open("results-VHDL.txt", 'r') as file_vhdl:
             return compare_results(file_c, file_vhdl, 0)
+
     _check_results()
 
 
