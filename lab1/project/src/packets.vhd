c662aa4c4e0c25666715510b8ccb323fc3aabca3
levnikolaevicmiskyn <37081598+levnikolaevicmiskyn@users.noreply.github.com>
2020-11-08 11:37:01 +0100

    Cleanup code in packets.vhd

--- lab1/project/src/packets.vhd
+++ lab1/project/src/packets.vhd
@@ -3,23 +3,10 @@ use ieee.numeric_std.all;
 use ieee.std_logic_1164.all;
 
 package packets is
---	type sigarray is array (natural range <>) of signed(constants.NBINT-1 downto 0);
-	
---	function pack(constant nrows: natural; constant bitwidth: natural; signal in_array: sigarray) return std_logic_vector;
 	function extract(signal insig : std_logic_vector; constant index: natural; constant bitwidth: natural) return signed;
 end packets;
 
 package body packets is
---	function pack(signal y: out std_logic_vector; constant nrows: natural; constant bitwidth: natural; signal in_array: sigarray) return sigarray is
---		variable y: std_logic_vector(nrows*bitwidth-1 downto 0);
---		begin
---			for i in 0 to in_array'length-2 loop
---				y((i+1)*bitwidth-1 downto i*bitwidth) := std_logic_vector(in_array(i));
---			end loop;
---			
---			return y;
---	end function pack;
-	
 	function extract(signal insig : std_logic_vector; constant index: natural;  constant bitwidth: natural) return signed is
 		variable y: signed(bitwidth-1 downto 0);
 		begin
