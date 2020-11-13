2425f97ca4473070aa635b1d225ab29372493d24
levnikolaevicmiskyn <37081598+levnikolaevicmiskyn@users.noreply.github.com>
2020-11-13 14:20:25 +0100

    Commented retiming script

--- lab1/auxiliary_files/retiming_algorithm.m
+++ lab1/auxiliary_files/retiming_algorithm.m
@@ -1,21 +1,32 @@
+%% Retiming algorithm
 % This script finds a solution to the retiming problem by means of an
 % auxiliary constraint graph
+% Usage: Each inequality in the form
+% r(i) - r(j) + d(ij) >= C(ij)
+% is added to the system by adding 
+% - a row (i,j) in the edges matrix
+% - d(ij) in the corresponding index in vector D
+% - C(ij) in the vector C0
 
 %% Step 1: Build the inequality matrix
-N=7; % Number of nodes
+N=7; % Number of nodes (operators)
+% edges is a Nx2 matrix containing the list of directed edges where (i;j)
+% corresponds to edge i->j
 edges = [1 8; 2 8; 2 1; 3, 2; 3, 4; 4, 3; 5, 3; 6, 3; 7, 5; 7, 6];
+
 Delta=2; % Number of pipeline stages
-D = [Delta, Delta, 1, 0, 0, 2, 1, 2, 0, 0]'; % Delay list
+
+% The number of registers (delays) on each edges are listed in vector D
+D = [Delta, Delta, 1, 0, 0, 2, 1, 2, 0, 0]';
 
 E=size(edges, 1);
-%A=zeros(E, N+1);
-%A(edges(:, 1)) = -1;
-%A(edges(:, 2)) = 1;
-%A=A(E, 1:N);
+% Omega is one if the path containing two adders is considered critical and
+% zero otherwise.
 omega = 1;
 C0 = [0, 0, 1, omega, 1, 1, 1, 1, 1, 1]';
+
 W = D-C0; % Weights
-%% Build the constraint graph
+%% Step 2: Build the constraint graph
 % Adjacency matrix
 M=Inf*ones(N+1, N+1);
 for ii = 1:size(edges, 1)
@@ -23,7 +34,7 @@ for ii = 1:size(edges, 1)
 end
 M(N+1, 1:N) = 0;
 
-%% Run Floyd Warshall algorithm
+%% Step 4: Run Floyd Warshall algorithm on the constraint graph
 M={M};
 for ii = 1:N+1
     Mp = Inf*ones(N+1, N+1);
@@ -37,6 +48,8 @@ for ii = 1:N+1
     end
     M{ii+1} = Mp;
     if any(diag(Mp) < 0)
+        % If any of the diagonal elements in matrix Mp is negative, there
+        % is a negative cycle and this algorithm fails to find a solution
         disp(['Negative cycle ' int2str(ii+1)])
     end
 end
