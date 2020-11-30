5588421140156ff77ec7950683a51bcd98457c3d
Gabryperr <lele.gabri1997@gmail.com>
2020-11-30 14:46:30 +0100

    Important logs

--- lab2/project/syn/synthesize.sh
+++ lab2/project/syn/synthesize.sh
@@ -4,5 +4,5 @@ rm -rf work
 echo "Initializing Synopsys..."
 source /software/scripts/init_synopsys_64.18
 echo "Running synthesis script..."
-dc_shell-xg-t "source synthesize.do; exit" > log-synth.txt
+dc_shell-xg-t -x "source synthesize.do; exit" > log-synth-standard.txt
 echo "Results available in reports/multiplier_pipeline/, reports/multiplier_pipeline_CSA, reports/multiplier_pipeline_pparch"
--- lab2/project/syn/synthesize_stage2_pipe.sh
+++ lab2/project/syn/synthesize_stage2_pipe.sh
@@ -3,17 +3,17 @@ rm -rf work
 echo "Initializing Synopsys..."
 source /software/scripts/init_synopsys_64.18
 echo "Running synthesis script (NPIPE=1)..."
-dc_shell-xg-t "source synthesize_stage2_pipe.do;" > log-synth_NPIPE1.txt
+dc_shell-xg-t -x "source synthesize_stage2_pipe.do; exit" > log-synth_NPIPE1.txt
 echo "Results available in reports/multiplier_stage2_pipelined"
 
 echo "Running synthesis script (NPIPE=2)..."
-dc_shell-xg-t "source synthesize_stage2_pipe_NPIPE2.do; exit" > log-synth_NPIPE2.txt
+dc_shell-xg-t -x "source synthesize_stage2_pipe_NPIPE2.do; exit" > log-synth_NPIPE2.txt
 echo "Results available in reports/multiplier_stage2_pipelined_NPIPE2"
 
 echo "Running synthesis script (NPIPE=4)..."
-dc_shell-xg-t "source synthesize_stage2_pipe_NPIPE4.do; exit" > log-synth_NPIPE4.txt
+dc_shell-xg-t -x "source synthesize_stage2_pipe_NPIPE4.do; exit" > log-synth_NPIPE4.txt
 echo "Results available in reports/multiplier_stage2_pipelined_NPIPE4"
 
 echo "Running synthesis script (NPIPE=6)..."
-dc_shell-xg-t "source synthesize_stage2_pipe_NPIPE6.do; exit" > log-synth_NPIPE6.txt
+dc_shell-xg-t -x "source synthesize_stage2_pipe_NPIPE6.do; exit" > log-synth_NPIPE6.txt
 echo "Results available in reports/multiplier_stage2_pipelined_NPIPE6"
