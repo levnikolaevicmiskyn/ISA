812d309518c0b1e52a458cdbf147df7c3018a6cb
Alessandro Scisca <s276032@studenti.polito.it>
2020-12-04 11:43:42 +0000

    Read standard input by default

--- lab3/project/sim/ALU/src/main.cpp
+++ lab3/project/sim/ALU/src/main.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
-#include <fstream>
 #include "../inc/ALU.h"
 
 int main() {
     ALU alu;
-    std::ifstream infile;
-    infile.open("inputs.txt");
-    alu.executeStream(infile, std::cout);
+    alu.executeStream(std::cin, std::cout);
     return 0;
 }
