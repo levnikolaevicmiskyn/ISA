dc797858f0ec4402a7c45ea5fa4fb64a72880347
shishka0 <s276032@studenti.polito.it>
2021-01-08 13:50:30 +0100

    Fix type incoherence

--- lab3/project/src/EX/EXController.vhd
+++ lab3/project/src/EX/EXController.vhd
@@ -7,7 +7,7 @@ use work.ALUpkg;
 
 entity EXController is
     port (
-        instruction_type: in globals.t_ALUOperandsType;
+        instruction_type: in globals.t_ALUInstructionType;
         sel_forward1: in std_logic_vector(1 downto 0);
         sel_forward2: in std_logic_vector(1 downto 0);
 
