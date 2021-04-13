db64ad930a3edf0867569bcbf203fece1d983740
Gabryperr <lele.gabri1997@gmail.com>
2021-04-13 18:59:32 +0200

    fixed dadda result type

--- lab4/fpmul/tb/refmod.sv
+++ lab4/fpmul/tb/refmod.sv
@@ -6,7 +6,7 @@ class refmod extends uvm_component;
     uvm_get_port #(packet_in) in;
     uvm_put_port #(packet_out) out;
 
-	logic[31:0] A1, A2, B1, B2, delayed_Z;
+	logic[31:0] A[1:2], B[1:2], delayed_Z;
     function new(string name = "refmod", uvm_component parent);
         super.new(name, parent);
         in = new("in", this);
@@ -20,23 +20,24 @@ class refmod extends uvm_component;
     
     virtual task run_phase(uvm_phase phase);
         super.run_phase(phase);
-        	A2 = 0;
-			B2 = 0;
-			A1 = 0;
-			B1 = 0;
+        	A = '{default:0};
+			//B2 = 0;
+			//A1 = 0;
+			B = '{default:0};
         forever begin
             in.get(tr_in);
             //tr_out.data = tr_out.data1;
 			//tr_out.data1 = tr_out.data2;
-			A2 <= A1;
-			B2 <= B1;
-			A1 <= tr_in.A;
-			B1 <= tr_in.B;
+			// Shift register to implement delay (due to pipelining)
+			A[2] <= A[1];
+			B[2] <= B[1];
+			A[1] <= tr_in.A;
+			B[1] <= tr_in.B;
 
-			tr_out.data = $shortrealtobits($bitstoshortreal(A1) * $bitstoshortreal(B1));		
+			tr_out.data = $shortrealtobits($bitstoshortreal(A[1]) * $bitstoshortreal(B[1]));		
 			delayed_Z <= tr_out.data;
-            $display("refmod: input A = %f, input B = %f, output OUT = %f",$bitstoshortreal(A2), $bitstoshortreal(B2), $bitstoshortreal(delayed_Z));
-			$display("refmod: input A = %b, input B = %b, output OUT = %b", A2, B2, delayed_Z);
+            $display("refmod: input A = %f, input B = %f, output OUT = %f",$bitstoshortreal(A[2]), $bitstoshortreal(B[2]), $bitstoshortreal(delayed_Z));
+			$display("refmod: input A = %b, input B = %b, output OUT = %b", A[2], B[2], delayed_Z);
             out.put(tr_out);
         end
     endtask: run_phase
